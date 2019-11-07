b5ed9d327a95b562832a0b71f9a81250c0b86627
fed <korolevfedos@yandex.ru>
2019-11-08 10:51:17 +1200

    test from atom

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  //res.redirect('/catalog'); // ПЕРЕАДРЕСАЦИЯ
-  
+  res.redirect('/catalog'); // ПЕРЕАДРЕСАЦИЯ
+
 });
 
 module.exports = router;
